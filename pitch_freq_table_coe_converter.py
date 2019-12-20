4d9542a124f1e71d8ba6aca0299571c6904b16a2
HowyoungZhou <howyoung3@gmail.com>
2019-12-20 22:39:58 +0800

    Create ROM for pitch frequencies table

---
+++ pitch_freq_table_coe_converter.py
@@ -0,0 +1,6 @@
+with open('pitch_freq_table.txt', 'r') as f:
+    with open('pitch_frep_table.coe', 'w') as coe_file:
+        coe_file.write('memory_initialization_radix = 10;\n')
+        coe_file.write('memory_initialization_vector = \n')
+        coe_file.write(',\n'.join(
+            [str(round(1 / float(x) * 1e9)) for x in f.read().split()]))
